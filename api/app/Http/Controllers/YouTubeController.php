2f7cd661a71819c087ab1396a66734c931900538
Roman Bestfator <rbest1981@gmail.com>
2021-09-01 08:41:58 +0300

    some edit

--- api/app/Http/Controllers/YouTubeController.php
+++ api/app/Http/Controllers/YouTubeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Validator;
 
 class YouTubeController extends Controller
 {
